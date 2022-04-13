ad9aab5013f5eabc5824c2dbb24d98e2fbcd5df0
Stepan Marek <stepan.marek@karlov.mff.cuni.cz>
2022-04-13 12:59:50 +0200

    Added color cycle to scatter plot

--- src/compphysutils/graphics/plot_types/scatter.py
+++ src/compphysutils/graphics/plot_types/scatter.py
@@ -2,5 +2,5 @@ def plot(datasets, axisObj, datasetLabels=False, **plotOptions):
     if not datasetLabels:
         datasetLabels = [None] * len(datasets)
     for dataIndex in range(len(datasets)):
-        axisObj.scatter(datasets[dataIndex][0], datasets[dataIndex][1], label=datasetLabels[dataIndex])
+        axisObj.scatter(datasets[dataIndex][0], datasets[dataIndex][1], label=datasetLabels[dataIndex], c=next(plotOptions["colorCycle"]))
     return axisObj
