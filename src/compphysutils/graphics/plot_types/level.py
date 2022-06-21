c7d371e92cc3dfcd9af5a50a0ccc99ac78600f42
Stepan Marek <stepan.marek@karlov.mff.cuni.cz>
2022-06-21 15:44:26 +0200

    Setting custom linelength of level plot

--- src/compphysutils/graphics/plot_types/level.py
+++ src/compphysutils/graphics/plot_types/level.py
@@ -1,7 +1,13 @@
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument("--linelength", default=1.0, type=float, help="Length of the line marking the level.")
+
 def plot(datasets, axisObj, datasetLabels=False, **plotOptions):
+    args = ap.parse_args(plotOptions["plotArgString"])
     if not datasetLabels:
         datasetLabels = [None] * len(datasets)
     for dataIndex in range(len(datasets)):
         # For x positions, only takes into account first element
-        axisObj.eventplot(datasets[dataIndex][1], lineoffsets=datasets[dataIndex][0][0], orientation="vertical", label=datasetLabels[dataIndex], colors=next(plotOptions["colorCycle"]))
+        axisObj.eventplot(datasets[dataIndex][1], lineoffsets=datasets[dataIndex][0][0], linelengths=args.linelength, orientation="vertical", label=datasetLabels[dataIndex], colors=next(plotOptions["colorCycle"]))
     return axisObj
