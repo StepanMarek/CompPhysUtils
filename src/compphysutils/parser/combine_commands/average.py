431ef42ab1c18743728ab8dcfae5049f87b7a67e
Stepan Marek <izzet.stmr@gmail.com>
2024-11-28 16:54:08 +0100

    Description of averaging program

--- src/compphysutils/parser/combine_commands/average.py
+++ src/compphysutils/parser/combine_commands/average.py
@@ -1,6 +1,6 @@
 import argparse
 
-averageAP = argparse.ArgumentParser()
+averageAP = argparse.ArgumentParser(prog="average", help="Element-wise average of the columns")
 averageAP.add_argument("--stderr", action="store_true", help="If given, second column of the new dataset includes standard error given by the average. Need at least two averaging datasets.")
 averageAP.add_argument("new_name", help="Name of the dataset containing the averaged values.")
 averageAP.add_argument("datasets_to_average", nargs="+", help="Datasets that are to be averaged, elementwise in each column. Number of columns is given by the first dataset.")
