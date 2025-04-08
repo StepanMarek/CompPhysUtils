797fc8d842d5343e4e7f847946a53e2f5447061d
Stepan Marek <izzet.stmr@gmail.com>
2025-04-08 13:22:14 +0200

    Skipping - simplest header solution

--- src/compphysutils/parser/parsers/cols.py
+++ src/compphysutils/parser/parsers/cols.py
@@ -6,11 +6,19 @@ class ColsReference:
     def __init__(self, parserArgs=["0"]):
         self.argsReader = argparse.ArgumentParser(description="Parses the arguments for the parser itself.")
         self.argsReader.add_argument("--comment", help="Character to be used as indicator of comment. [default : #]", default="#")
+        self.argsReader.add_argument("--skip", type=int, default=0, help="Skip a given number of lines before starting the read.")
         self.argsReader.add_argument("cols", nargs="+", type=int, help="Indices of columns to read.")
         self.args = self.argsReader.parse_args(parserArgs)
+        self.skips_remaining = self.args.skip
 
     def continueReading(self, line):
         stripLine = line.strip()
+        if self.skips_remaining > 0:
+            self.skips_remaining -= 1
+            return False
+        if len(stripLine) == 0:
+            # Do not attempt to read empty lines
+            return False
         if stripLine[0] == self.args.comment:
             return False
         # Not commented, read
