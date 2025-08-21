f393a9070b6257a32b248b1acbd9d17fe9475bc6
Stepan Marek <izzet.stmr@gmail.com>
2025-08-21 13:30:07 +0200

    Marker sizes in loglog and scatter graphs

--- src/compphysutils/graphics/plot_types/scatter.py
+++ src/compphysutils/graphics/plot_types/scatter.py
@@ -1,6 +1,13 @@
+import argparse
+
+ap = argparse.ArgumentParser(prog="scatter", description="Scatter graph.")
+ap.add_argument("--ms", default=20.0, type=float, help="Marker size.")
+
 def plot(datasets, axisObj, datasetLabels=False, **plotOptions):
+    args = ap.parse_args(plotOptions["plotArgString"])
     if not datasetLabels:
         datasetLabels = [None] * len(datasets)
     for dataIndex in range(len(datasets)):
-        axisObj.scatter(datasets[dataIndex][0], datasets[dataIndex][1], label=datasetLabels[dataIndex], c=next(plotOptions["colorCycle"]), marker=next(plotOptions["markerstyleCycle"]))
+        axisObj.scatter(datasets[dataIndex][0], datasets[dataIndex][1], label=datasetLabels[dataIndex], c=next(plotOptions["colorCycle"]), marker=next(plotOptions["markerstyleCycle"]),
+                        s=args.ms)
     return axisObj
