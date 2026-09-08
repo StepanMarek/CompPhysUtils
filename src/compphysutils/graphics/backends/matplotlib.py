de9e1b6b9dc2609b7512bb085be27f8f6bfc4add
Stepan Marek <stepan.marek>
2026-09-09 01:45:31 +0200

    Matplotlib : Scatter implementation + some plot settings

--- src/compphysutils/graphics/backends/matplotlib.py
+++ src/compphysutils/graphics/backends/matplotlib.py
@@ -10,12 +10,26 @@ class Figure(FigureBase):
         self._figure = matplotlib.figure.Figure()
 
     def save(self, name):
+        for i in range(len(self.axes)):
+            self.axes[i].save()
         self._figure.savefig(name)
 
 class Axes(AxesBase):
     def __init__(self, figure):
         super().__init__(figure)
-        self._axes = figure._figure.add_axes((0.0, 0.0, 1.0, 1.0))
+        self._axes = figure._figure.add_subplot(1,1,1)
+        figure.axes.append(self)
+
+    def save(self):
+        if self.labels[0]:
+            self._axes.set_xlabel(self.labels[0])
+        if self.labels[1]:
+            self._axes.set_ylabel(self.labels[1])
+        self._axes.legend()
 
     def plot(self, x, y, label=False, color=False, linestyle=False):
         self._axes.plot(x, y, label=label, color=color, linestyle=linestyle)
+
+    def scatter(self, x, y, label=False, color=False, markerstyle=False, linestyle="-"):
+        self._axes.scatter(x, y,
+                           label=label, c=color, marker=markerstyle, linestyle=linestyle)
